de0e622d8ee175a277b0024b2e2b477031121202
David Litvak Bruno <david.litvakb@gmail.com>
2012-07-03 22:07:00 -0300

    forgot to update version number file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mongomodels",
-    version="0.1.0",
+    version="0.1.2",
     description="A simple ORM for MongoDB",
     author="David Litvak",
     author_email = "david.litvakb@gmail.com",
