098997a45f812f44790c4705004b26e443714b40
David Litvak Bruno <david.litvakb@gmail.com>
2013-02-13 20:18:29 -0300

    new minor version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mongomodels",
-    version="0.7.7",
+    version="0.7.8",
     description="A simple ODM for MongoDB",
     author="David Litvak",
     author_email = "david.litvakb@gmail.com",
